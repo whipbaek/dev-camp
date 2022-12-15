9deb2f7e8f12617af08e6247f973c8387fa095f4
whibaek <jibaek0513@gmail.com>
2022-12-16 02:22:47 +0900

    feat(frontend) : 리액트 파일 초기화

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [message, setMessage] = useState([]);
+    useEffect(() => {
+        fetch("/test")
+            .then((response) => {
+                return response.json();
+            })
+            .then(function (data) {
+                setMessage(data);
+            });
+    }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+          <ul>
+              {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
+          </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
