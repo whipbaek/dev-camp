753cd8fa230ab8902965afbbc9f7fe6e8543874a
whibaek <jibaek0513@gmail.com>
2022-12-17 03:19:22 +0900

    feat : axios 통신 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from "react";
+import axios, {Axios} from "axios";
 
 function App() {
-    const [message, setMessage] = useState([]);
-    useEffect(() => {
-        fetch("/test")
-            .then((response) => {
-                return response.json();
+    const [str, setStr] = useState("");
+
+    useEffect(()=>{
+        axios.get("/api/test")
+            .then((response) =>{
+                console.log(response.data);
+                setStr(response.data);
             })
-            .then(function (data) {
-                setMessage(data);
-            });
+            .catch(() =>{
+                alert("fail");
+            })
     }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <ul>
-              {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-          </ul>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Data : {str}</h1>
+            </header>
+        </div>
+    );
 }
+
+
+
+// function App() {
+//     const [message, setMessage] = useState([]);
+//     useEffect(() => {
+//         fetch("/test")
+//             .then((response) => {
+//                 return response.json();
+//             })
+//             .then(function (data) {
+//                 setMessage(data);
+//             });
+//     }, []);
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//           <ul>
+//               {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
+//           </ul>
+//       </header>
+//     </div>
+//   );
+// }
+
 
 export default App;
--- src/main/java/com/smilegate/devcamp/controller/testController.java
+++ src/main/java/com/smilegate/devcamp/controller/testController.java
@@ -8,8 +8,13 @@ import java.util.List;
 
 @RestController
 public class testController {
-    @GetMapping("/test")
-    public List<String> test() {
-        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+//    @GetMapping("/test")
+//    public List<String> test() {
+//        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+//    }
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "서버에서 넘어온 데이터 입니다.";
     }
 }
