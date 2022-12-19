2648e6e50a0767de5c16f11f9fd5b3287b278be7
whibaek <jibaek0513@gmail.com>
2022-12-19 15:02:09 +0900

    feat() : component 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,41 +46,4 @@ function App() {
     );
 }
 
-
-
-// function App() {
-//     const [message, setMessage] = useState([]);
-//     useEffect(() => {
-//         fetch("/test")
-//             .then((response) => {
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 setMessage(data);
-//             });
-//     }, []);
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//           <ul>
-//               {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-//           </ul>
-//       </header>
-//     </div>
-//   );
-// }
-
-
 export default App;
