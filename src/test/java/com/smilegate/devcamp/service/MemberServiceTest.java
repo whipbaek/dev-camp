f25b2b807332dfb0c5f2745004ce69e632094f07
whibaek <jibaek0513@gmail.com>
2022-12-21 19:59:28 +0900

    test : 비밀번호 암호화 테스트

--- src/test/java/com/smilegate/devcamp/service/MemberServiceTest.java
+++ src/test/java/com/smilegate/devcamp/service/MemberServiceTest.java
@@ -3,6 +3,7 @@ package com.smilegate.devcamp.service;
 import com.smilegate.devcamp.dto.MemberDto;
 import com.smilegate.devcamp.entity.Member;
 import com.smilegate.devcamp.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
@@ -11,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,13 +26,14 @@ public class MemberServiceTest {
     @DisplayName("회원가입 테스트 입니다.")
     public void 회원가입() {
         //given -> 멤버가 주어짐
-        Member member = new Member("whip", "1234", "whipbaek");
+        MemberDto memberdto = new MemberDto("whipbaek@gmail.com", "tempPassword", "whipbaek");
+
         //when -> 가입하면
-        Long count = memberService.join(new MemberDto(member));
+        Long count = memberService.join(memberdto);
         System.out.println("count = " + count);
 
         //then -> DB 접근하여 id 값이 같은지 확인해보자 (dto를 거치므로 객체는 다르게 저장됨)
-        assertEquals(member.getId(), memberRepository.findOne(count).getId());
+        assertThat(memberdto.getId()).isEqualTo(memberRepository.findOne(count).getId());
 
     }
 }
